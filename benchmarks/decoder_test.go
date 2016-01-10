6fd15609e2363ecbba35790ad3fbf2b0820aad2b
Àlex Vinyals Valdepeñas <alevinval@gmail.com>
2016-01-10 23:02:40 +0100

    Update benchmarks to be more dry.

--- benchmarks/decoder_test.go
+++ benchmarks/decoder_test.go
@@ -7,13 +7,14 @@ import (
 	"time"
 )
 
-func doBenchmark(b *testing.B, data []byte) {
+func doDecodingBenchmark(b *testing.B, data []byte) {
 	data = append(data, data...)
 	reader := bytes.NewReader(data)
 	decoder := sse.NewDecoder(65536)
 	events := decoder.Decode(reader)
 	time.Sleep(100 * time.Millisecond)
 
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		reader.Seek(0, 0)
 		<-events
@@ -21,39 +22,34 @@ func doBenchmark(b *testing.B, data []byte) {
 }
 
 func BenchmarkDecodeEmptyEvent(b *testing.B) {
-	b.ReportAllocs()
 	event := []byte("data: \n\n")
-	doBenchmark(b, event)
+	doDecodingBenchmark(b, event)
 }
 
 func BenchmarkDecodeEmptyEventWithIgnoredLine(b *testing.B) {
-	b.ReportAllocs()
 	event := []byte(":ignored line \n\ndata: \n\n")
-	doBenchmark(b, event)
+	doDecodingBenchmark(b, event)
 }
 
 func BenchmarkDecodeShortEvent(b *testing.B) {
-	b.ReportAllocs()
 	event := []byte("data: short event\n\n")
-	doBenchmark(b, event)
+	doDecodingBenchmark(b, event)
 }
 
 func BenchmarkDecode8kEvent(b *testing.B) {
-	b.ReportAllocs()
 	event := []byte("data: ")
 	for x := 0; x < 8192; x++ {
 		event = append(event, []byte("e")...)
 	}
 	event = append(event, []byte("\n\n")...)
-	doBenchmark(b, event)
+	doDecodingBenchmark(b, event)
 }
 
 func BenchmarkDecode16kEvent(b *testing.B) {
-	b.ReportAllocs()
 	event := []byte("data: ")
 	for x := 0; x < 16384; x++ {
 		event = append(event, []byte("e")...)
 	}
 	event = append(event, []byte("\n\n")...)
-	doBenchmark(b, event)
+	doDecodingBenchmark(b, event)
 }
