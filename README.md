7e1f30251c72771bd45d91f45a5dd064cb28dbad
Alex Vinyals <alevinval@gmail.com>
2016-10-20 19:31:35 +0200

    benchmarks: were not accurate and wrong, they are better now.

--- README.md
+++ README.md
@@ -17,16 +17,3 @@ Library to consume SSE streams according to the HTML5 standard.
 
 `cd $GOPATH/src/github.com/mubit/sse/benchmarks && go test -bench=.`
 
-### Benchmarks
-
-````
-BenchmarkDecodeEmptyEvent-4                  	 2000000	       804 ns/op	      64 B/op	       1 allocs/op
-BenchmarkDecodeEmptyEventWithIgnoredLine-4   	 2000000	       892 ns/op	      64 B/op	       1 allocs/op
-BenchmarkDecodeShortEvent-4                  	 2000000	       898 ns/op	      80 B/op	       2 allocs/op
-BenchmarkDecode8kEvent-4                     	  200000	      6199 ns/op	    8256 B/op	       2 allocs/op
-BenchmarkDecode16kEvent-4                    	  100000	     10309 ns/op	   16450 B/op	       2 allocs/op
-PASS
-ok  	github.com/mubit/sse/benchmarks	12.341s
-````
-
-Benchmarked against a MacBook Air 2013 (1,3 GHz Intel Core i5, 8 GB 1600 MHz DDR3)
--- benchmarks/decoder_test.go
+++ benchmarks/decoder_test.go
@@ -3,54 +3,56 @@ package benchmarks_test
 import (
 	"bytes"
 	"testing"
-	"time"
 
 	"github.com/mubit/sse"
 )
 
-func doDecodingBenchmark(b *testing.B, data []byte) {
-	data = append(data, data...)
+func runDecodingBenchmark(b *testing.B, data []byte) {
 	reader := bytes.NewReader(data)
-	decoder := sse.NewDecoder(65536)
-	events := decoder.Decode(reader)
-	time.Sleep(100 * time.Millisecond)
-
-	b.ReportAllocs()
+	decoder := sse.NewDecoder(reader)
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		decoder.Decode()
 		reader.Seek(0, 0)
-		<-events
 	}
 }
 
 func BenchmarkDecodeEmptyEvent(b *testing.B) {
 	event := []byte("data: \n\n")
-	doDecodingBenchmark(b, event)
+	runDecodingBenchmark(b, event)
 }
 
 func BenchmarkDecodeEmptyEventWithIgnoredLine(b *testing.B) {
 	event := []byte(":ignored line \n\ndata: \n\n")
-	doDecodingBenchmark(b, event)
+	runDecodingBenchmark(b, event)
 }
 
 func BenchmarkDecodeShortEvent(b *testing.B) {
 	event := []byte("data: short event\n\n")
-	doDecodingBenchmark(b, event)
+	runDecodingBenchmark(b, event)
+}
+
+func createEventWithPadding(size int) []byte {
+	event := []byte("data: ")
+	paddingByte := byte('e')
+	for x := 0; x < size-8; x++ {
+		event = append(event, paddingByte)
+	}
+	return append(event, []byte("\n\n")...)
+}
+
+func BenchmarkDecode1kEvent(b *testing.B) {
+	runDecodingBenchmark(b, createEventWithPadding(1000))
+}
+
+func BenchmarkDecode4kEvent(b *testing.B) {
+	runDecodingBenchmark(b, createEventWithPadding(4000))
 }
 
 func BenchmarkDecode8kEvent(b *testing.B) {
-	event := []byte("data: ")
-	for x := 0; x < 8192; x++ {
-		event = append(event, []byte("e")...)
-	}
-	event = append(event, []byte("\n\n")...)
-	doDecodingBenchmark(b, event)
+	runDecodingBenchmark(b, createEventWithPadding(8000))
 }
 
 func BenchmarkDecode16kEvent(b *testing.B) {
-	event := []byte("data: ")
-	for x := 0; x < 16384; x++ {
-		event = append(event, []byte("e")...)
-	}
-	event = append(event, []byte("\n\n")...)
-	doDecodingBenchmark(b, event)
+	runDecodingBenchmark(b, createEventWithPadding(16000))
 }
