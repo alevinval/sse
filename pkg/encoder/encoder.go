63ba29af0f2bbf84aacac034d36ae283413542d0
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2022-03-18 09:15:10 +0100

    encoder: optimise encoder
    name                old time/op  new time/op  delta
    EncodeEmptyEvent-8  14.7ns ± 0%  11.9ns ± 0%  -19.44%  (p=0.008 n=5+5)
    EncodeShortEvent-8  56.0ns ± 0%  23.3ns ± 0%  -58.35%  (p=0.008 n=5+5)
    Encode1kEvent-8      191ns ± 1%    56ns ± 0%  -70.86%  (p=0.008 n=5+5)
    Encode4kEvent-8      649ns ± 2%   152ns ± 1%  -76.60%  (p=0.008 n=5+5)
    Encode8kEvent-8     1.32µs ± 4%  0.28µs ± 0%  -78.55%  (p=0.008 n=5+5)
    Encode16kEvent-8    2.32µs ± 1%  0.57µs ± 1%  -75.31%  (p=0.016 n=4+5)

--- pkg/encoder/encoder.go
+++ pkg/encoder/encoder.go
@@ -24,24 +24,32 @@ func (e *Encoder) Write(event base.MessageEventGetter) (int, error) {
 	e.buf.Reset()
 
 	if event.GetLastEventID() != "" {
-		e.buf.WriteString("id: " + event.GetLastEventID() + "\n")
+		e.buf.WriteString("id: ")
+		e.buf.WriteString(event.GetLastEventID())
+		e.buf.WriteByte('\n')
 	}
 
 	if event.GetName() != "" {
-		e.buf.WriteString("name: " + event.GetName() + "\n")
+		e.buf.WriteString("name: ")
+		e.buf.WriteString(event.GetName())
+		e.buf.WriteByte('\n')
 	}
 
 	if event.GetData() != "" {
-		e.buf.WriteString("data: " + event.GetData() + "\n")
+		e.buf.WriteString("data: ")
+		e.buf.WriteString(event.GetData())
+		e.buf.WriteByte('\n')
 	}
 
-	e.buf.WriteString("\n")
+	e.buf.WriteByte('\n')
 
 	return e.out.Write(e.buf.Bytes())
 }
 
 func (e *Encoder) SetRetry(retryDelayInMillis int) {
 	e.buf.Reset()
-	e.buf.WriteString("retry: " + strconv.Itoa(retryDelayInMillis) + "\n")
+	e.buf.WriteString("retry: ")
+	e.buf.WriteString(strconv.Itoa(retryDelayInMillis))
+	e.buf.WriteByte('\n')
 	e.out.Write(e.buf.Bytes())
 }
